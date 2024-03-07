4cfe80158490f1b1117e63922705a95818f0f6cf
Misha Dvali <mykhailo.dvali@gmail.com>
2024-03-07 17:34:35 +0200

    Grid created

---
+++ Grid.py
@@ -0,0 +1,25 @@
+from typing import List
+
+import SudokuLike
+from SudokuLike import *
+class Grid:
+    def __init__(self, unsolved:List[List[Cell]], shapes:List[Shape]):
+        self.initialBoard:List[List[Cell]] = unsolved
+        self.board:List[List[Cell]] = unsolved #todo to copy an array, not the link
+        self.shapes:List[Shape] = shapes
+
+    def solve(self) -> Grid:
+         raise NotImplementedError()
+         #todo
+
+    def reset(self) -> None:
+        raise NotImplementedError #todo
+
+    def insert(self, val:int, x:int, y:int) -> None:
+        self.board[x][y].setTo(val)
+
+    def isValid(self) -> bool:
+        for shape in self.shapes:
+            if not shape.rules.isValid(shape):
+                return False
+        return True
